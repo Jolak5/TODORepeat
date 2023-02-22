53a94827553c2f625ed560e58ffa4f41a9673fea
jolak5 <kayodej4@gmail.com>
2023-02-23 00:33:02 +0100

    Create webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+import _ from 'lodash';
+import './style.css';
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo list',
+      template: './src/index.html',
+    }),
+  ],
+};
