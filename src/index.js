960866c0aaeb044de52cfd374c3fa5106304e3d2
jolak5 <kayodej4@gmail.com>
2023-02-23 00:40:52 +0100

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const list = document.querySelector('ul');
 function createList() {
   list.insertAdjacentHTML(
     'afterbegin',
-    ' <hr > <div class="single-list"><div class="li-div"> <input type="checkbox" name="check" value="checkbox"> <li class="list-item"> </div><img src="/dots.svg" alt=""><i class="fa-solid fa-ellipsis-vertical fa-2x"></i></div>'
+    ' <hr > <div class="single-list"><div class="li-div"> <input type="checkbox" name="check" value="checkbox"> <li class="list-item"> </div><img src="/dots.svg" alt=""><i class="fa-solid fa-ellipsis-vertical fa-2x"></i></div>',
   );
 }
 
